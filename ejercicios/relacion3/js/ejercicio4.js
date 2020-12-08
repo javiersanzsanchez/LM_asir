7bce219ad051537687067a3db67d518cab1080fc
Javier Sanz Sánchez <javiersanzsanchez@gmail.com>
2020-12-08 23:17:26 +0100

    relacion 3

---
+++ ejercicios/relacion3/js/ejercicio4.js
@@ -0,0 +1,17 @@
+function redimensionarIframe(obj) {
+    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+}
+
+function loadPaises(){
+    console.log("cargar paises");
+    $.ajax({
+        url: "https://restcountries.eu/rest/v2/all" // servicio con información de paises :)
+    }).done(function (data) {
+        //console.log(data);
+        data.forEach(function(pais) {
+            console.log(pais); // name numericCode
+            document.getElementById('sidebar').append('<option value="'+pais.numericCode+'">'+pais.name+'</option>');
+        });
+        document.getElementById("paises").selectpicker("refresh");
+    });
+}
